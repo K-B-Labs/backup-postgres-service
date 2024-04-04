190cc9c9407c7391a58bd730e7196f73ea17be1a
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-04-04 18:32:23 +0300

    Remove cron check as it doesn't verify robfig edge-case

--- cron/cron.go
+++ cron/cron.go
@@ -2,21 +2,15 @@ package cron
 
 import (
 	"fmt"
-	"os"
 	"postgres-backup-service/db"
 	"postgres-backup-service/env"
 
-	"github.com/adhocore/gronx"
 	"github.com/robfig/cron"
 )
 
 func StartDumpCronJob() {
 	fmt.Println("Starting cron service")
 
-	if !gronx.New().IsValid(env.ENVIRONMENT.BACKUP_CRON) {
-		fmt.Printf("Invalid cron expression: %s\n", env.ENVIRONMENT.BACKUP_CRON)
-		os.Exit(1)
-	}
 	c := cron.New()
 	c.AddFunc(env.ENVIRONMENT.BACKUP_CRON, db.Dump)
 	c.Start()
--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module postgres-backup-service
 go 1.22
 
 require (
-	github.com/adhocore/gronx v1.8.1
 	github.com/joho/godotenv v1.5.1
 	github.com/robfig/cron v1.2.0
 )
-
-require golang.org/x/tools v0.19.0 // indirect
